2d4129a99888bc889c38ceb34b60e52ebefbaa11
wonik-choi <wonik.choi@susimdal.com>
2024-04-23 00:46:23 +0900

    feat: Notification

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,14 @@ initializeApp(firebaseConfig);
 
 const messaging = getMessaging();
 
+Notification.requestPermission().then((permission) => {
+    if (permission === "granted") {
+        console.log("Notification permission granted.");
+    } else {
+        console.log("Unable to get permission to notify.");
+    }
+});
+
 onMessage(messaging, (payload) => {
     console.log("Message received. ", payload);
     // ...
